9f195589fb766de1018bdb7c94c244f02fc17b06
Miner448 <rx-andriy@ukr.net>
2023-05-03 19:22:34 +0200

    JS

---
+++ JS/main.js
@@ -0,0 +1,48 @@
+
+
+console.log(`hello word`)
+
+
+let i = `hello`
+console.log(i)
+let b = `owu`
+console.log(b)
+let c = `com`
+console.log(c)
+let d =`ua`
+console.log(d)
+let s = `1`
+console.log(s)
+let t = `10`
+console.log(t)
+let y = `-999`
+console.log(y)
+let j = `123`
+console.log(j)
+let k = `3.14`
+console.log(k)
+let p = `2.17`
+console.log(p)
+let q = `16`
+console.log(q)
+let n = `true`
+console.log(n)
+let m = `false`
+console.log(m)
+
+
+let firstname = `Dulibianyk`
+let middlename = `Andrii`
+let lastname = `Vasylevich`
+console.log(firstname,middlename,lastname)
+
+let a = `100`
+console.log(typeof a);
+
+let bo = '100';
+console.log(typeof bo);
+
+let co = true;
+console.log(typeof co);
+
+
